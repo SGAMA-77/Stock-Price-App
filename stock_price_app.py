9c9b41d5bf349e2606be4f58688e7c1c418f79f0
SGAMA-77 <kessie.sigama@gmail.com>
2025-02-25 21:19:06 +0200

    Initial commit

---
+++ stock_price_app.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import yfinance as yf
+import streamlit as st 
+
+st.write("""
+    # Stock Price App
+    
+    Shown are the stock closing price and volume of Nasdaq
+""")
+
+# Defining the ticker symbol
+ticker_symbol = 'GOOGL'
+
+# Getting data from the ticker symbol
+ticker_data = yf.Ticker(ticker_symbol)
+
+# Getting historical prices for the ticker
+ticker_period = ticker_data.history(period='1d', start='2010-5-31', end='2020-5-31')
+
