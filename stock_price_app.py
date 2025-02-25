3620907d08577f55295ed95398e3f18f986a1bd8
SGAMA-77 <kessie.sigama@gmail.com>
2025-02-25 22:01:30 +0200

    Updated code

--- stock_price_app.py
+++ stock_price_app.py
@@ -17,3 +17,5 @@ ticker_data = yf.Ticker(ticker_symbol)
 # Getting historical prices for the ticker
 ticker_period = ticker_data.history(period='1d', start='2010-5-31', end='2020-5-31')
 
+st.line_chart(ticker_period.Close)
+st.line_chart(ticker_period.Volume)
